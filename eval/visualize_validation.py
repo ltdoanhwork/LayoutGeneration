a82fdba5ddae24483dc6bdebe24d6323cdc13e6e
ltdoanhwork <lethiendoanh.work@gmail.com>
2025-12-05 05:05:13 +0000

    [UPdate] update docs

--- eval/visualize_validation.py
+++ eval/visualize_validation.py
@@ -485,4 +485,11 @@ python -m eval.visualize_validation \
     --val_output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_track_d_anime_reward_v2/val_runs\
     --output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_track_d_anime_reward_v2/val_runs/plots 
 
+python -m eval.visualize_validation \
+    --val_output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_rl_multi_video/val_runs\
+    --output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_rl_multi_video/val_runs/plots
+
+python -m eval.visualize_validation \
+    --val_output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_anime_premium_v1/val_runs\
+    --output_dir /home/serverai/ltdoanh/LayoutGeneration/runs/dsn_anime_premium_v1/val_runs/plots 
 """ 
